6b8028ee67b4c10cdcbbd96c53a31f3885694c9a
Steve Kiaie <skiaie@cloudera.com>
2020-05-22 23:39:08 +0000

    initial commit

---
+++ _SparkSession_fileAccess.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import Row, StructField, StructType, StringType, IntegerType
+
+spark = SparkSession.builder \
+     .master("local[*]") \
+   .config("spark.hadoop.fs.s3a.s3guard.ddb.region","us-east-2") \
+   .config("spark.yarn.access.hadoopFileSystems","s3a://cdp-sandbox-default-se") \
+   .config("spark.hadoop.yarn.resourcemanager.principal", "skiaie") \
+   .getOrCreate()  
+    
+    
+spark.sql("SHOW databases").show()
+  
+sql0 = """
+drop table students
+"""   
+
+
+sql1 = """
+CREATE EXTERNAL TABLE IF NOT EXISTS students(student_id INT)
+COMMENT 'Student Names'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION 's3a://cdp-sandbox-default-se/sbk-temp'
+"""    
+
+sql2 = """
+CREATE TABLE IF NOT EXISTS 
+students(student_id INT)
+ """
+
+sql3 = """
+alter table students ADD columns (dept String)
+"""
+
+#spark.sql(sql3).show()
+
+spark.sql(sql1).show()
+ 
+
+
+sql4 = """
+insert into students values (0, "math") 
+"""
+
+spark.sql(sql4).show()
+ 
+"""
+spark.sql(sql1).show()
+"""
